16c20243a3ee74ac68e6148d09904cab0d94b7dd
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-10-14 10:04:28 +0700

    add donation API
    - process all donation, can't refund
    
    other feature WIP

--- src/main/java/service/music/PremiumService.java
+++ src/main/java/service/music/PremiumService.java
@@ -1,15 +1,29 @@
 package service.music;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
-import database.GuildHistoryModel;
-import database.TrackModel;
-import database.UserHistoryModel;
+import database.*;
+import io.donatebot.api.DBClient;
+import io.donatebot.api.Donation;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class PremiumService
 {
+    private String apiKey;
+    private String serverID;
+
+    private String premiumUser = "li4NBOCVxk";
+
     public PremiumService()
     {
 
+    }
+
+    public PremiumService(String serverID, String apiKey)
+    {
+        this.apiKey = apiKey;
+        this.serverID = serverID;
     }
 
     /**
@@ -25,16 +39,91 @@ public class PremiumService
         trackModel.addTrack(title, url);
         long trackId = trackModel.getTrackId(url);
 
-        if (trackModel.isPremium(event.getGuild().getIdLong(), "GUILD"))
+        PremiumModel db = new PremiumModel();
+        if (db.isPremium(event.getGuild().getIdLong(), "GUILD"))
         {
             GuildHistoryModel guild = new GuildHistoryModel();
             guild.addGuildHistory(event.getGuild().getIdLong(), event.getAuthor().getIdLong(), trackId);
         }
 
-        if (trackModel.isPremium(event.getAuthor().getIdLong(), "USER"))
+        if (db.isPremium(event.getAuthor().getIdLong(), "USER"))
         {
             UserHistoryModel user = new UserHistoryModel();
             user.addUserHistory(event.getAuthor().getIdLong(), trackId);
         }
     }
+
+    /**
+     * Check donation API
+     */
+    public void check()
+    {
+        DBClient dbClient = new DBClient(this.serverID, this.apiKey);
+//        String[] statuses = {"Completed", "Reversed", "Refunded"};
+        String[] statuses = {"Completed"};
+        CompletableFuture<Donation[]> future = dbClient.getNewDonations(statuses);
+        PremiumModel db = new PremiumModel();
+
+        // step
+        // check product subscription
+        // - if new user/guild, add to database
+        // - if existing user/guild, renew subscription date
+        // (maybe) delete user/guild who not renew subscription over 1 month
+        // mark all successful transaction as "done"
+
+        CompletableFuture.runAsync(() ->
+        {
+            try
+            {
+                for (Donation item : future.get())
+                {
+                    boolean result = false;
+
+                    // user
+                    if (item.getSellerCustoms().get("Your In-Game ID").equals(this.premiumUser))
+                    {
+                        continue;
+                    }
+                    else if (item.getProductID().equals(this.premiumUser))
+                    {
+                        long userId = Long.parseLong(item.getBuyerID());
+                        if (db.isNew(userId, "USER"))
+                        {
+                            result = db.addPremium(userId, "USER");
+                        }
+                        else
+                        {
+                            result = db.renewSubscription(userId, "USER");
+                        }
+                    }
+                    // guild
+                    else
+                    {
+                        long guildId = Long.parseLong(item.getBuyerID());
+                        if (db.isNew(guildId, "GUILD"))
+                        {
+                            result = db.addPremium(guildId, "GUILD");
+                        }
+                        else
+                        {
+                            result = db.renewSubscription(guildId, "GUILD");
+                        }
+                    }
+
+                    // mark as done
+                    if (result)
+                    {
+                        dbClient.markDonation(item.getTransactionID(), true, true);
+                    }
+                }
+                System.out.println("Done");
+            }
+            catch (InterruptedException | ExecutionException e)
+            {
+                e.printStackTrace();
+            }
+        });
+    }
 }
+
+
