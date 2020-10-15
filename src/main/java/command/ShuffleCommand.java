65f45b9e214adbb7dc5aca8e7bbc412d81ea073f
MadeYoga <vngla21@gmail.com>
2020-10-15 21:08:22 +0700

    Add shuffle command

---
+++ src/main/java/command/ShuffleCommand.java
@@ -0,0 +1,47 @@
+package command;
+
+import client.NanoClient;
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import service.music.GuildMusicManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
+public class ShuffleCommand extends Command {
+
+    NanoClient nanoClient;
+
+    public ShuffleCommand(NanoClient nanoClient) {
+        this.nanoClient = nanoClient;
+
+        this.name = "shuffle";
+        this.aliases = new String[] {"shuffle_queue", "sq"};
+        this.guildOnly = true;
+        this.cooldown = 2;
+        this.category = new Category("Music");
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        GuildMusicManager musicManager = this.nanoClient.getGuildAudioPlayer(event.getGuild());
+
+        // Get current queue
+        BlockingQueue<AudioTrack> queue = musicManager.scheduler.getQueue();
+
+        // Add songs to List
+        List<AudioTrack> tracks = new ArrayList<>();
+        tracks.addAll(queue);
+
+        Collections.shuffle(tracks);
+
+        // Replace current queue
+        musicManager.scheduler.getQueue().clear();
+        musicManager.scheduler.getQueue().addAll(tracks);
+
+        event.getMessage().addReaction(event.getClient().getSuccess()).queue();
+    }
+}
