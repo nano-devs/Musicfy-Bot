25e5973690eeafa199b2f2e2c11ca1d3e6f65849
MadeYoga <vngla21@gmail.com>
2020-10-17 17:02:14 +0700

    Fix bug on loop mode

--- src/main/java/service/music/TrackScheduler.java
+++ src/main/java/service/music/TrackScheduler.java
@@ -69,7 +69,7 @@ public class TrackScheduler extends AudioEventAdapter {
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
         // If repeat true, then re-enqueue url.
         if (inLoopState) {
-            queue(track);
+            queue(track.makeClone());
         }
 
         // Only start the next track if the end reason is suitable for it (FINISHED or LOAD_FAILED)
