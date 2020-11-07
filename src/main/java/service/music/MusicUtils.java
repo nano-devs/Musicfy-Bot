5ca05657dc102456d3ffe0188990bd88cdb9205d
MadeYoga <vngla21@gmail.com>
2020-11-07 13:10:01 +0700

    Add dj methods

--- src/main/java/service/music/MusicUtils.java
+++ src/main/java/service/music/MusicUtils.java
@@ -1,5 +1,10 @@
 package service.music;
 
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+
 public class MusicUtils {
     /**
      *
@@ -29,5 +34,26 @@ public class MusicUtils {
         if (durationMinute.length() == 1) durationMinute = "0" + durationMinute;
         if (durationSecond.length() == 1) durationSecond = "0" + durationSecond;
         return durationMinute + ":" + durationSecond;
+    }
+
+    public static boolean hasDjRole(Member member) {
+        for (Role role : member.getRoles()) {
+            if (role.getName().toUpperCase().equals("DJ")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static EmbedBuilder getDjModeEmbeddedWarning(Member member) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(member.getColor());
+        embedBuilder.setAuthor(member.getUser().getName(),
+                member.getUser().getAvatarUrl(), member.getUser().getAvatarUrl());
+        embedBuilder.addField(":warning: Dj mode is on.",
+                "Only member with role `DJ` can modify the AudioPlayer state", true);
+        embedBuilder.setFooter("Dj mode will automatically be turned off when bot leave the voice channel");
+
+        return embedBuilder;
     }
 }
