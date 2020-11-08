d70c8da664ea902d789f62b2ff27a34562c98e24
MadeYoga <vngla21@gmail.com>
2020-11-08 22:43:21 +0700

    Add CustomEmbedBuilder

---
+++ src/main/java/service/music/CustomEmbedBuilder.java
@@ -0,0 +1,13 @@
+package service.music;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.awt.*;
+
+public class CustomEmbedBuilder extends EmbedBuilder {
+
+    public CustomEmbedBuilder() {
+        // Theme Color: RGB(211, 0, 137)
+        this.setColor(new Color(211, 0, 137));
+    }
+}
