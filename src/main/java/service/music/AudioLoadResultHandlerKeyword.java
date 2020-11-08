d5444ca56b58e87e7607c271a009d0be396cb6a7
MadeYoga <vngla21@gmail.com>
2020-11-08 16:17:17 +0700

    Update keyword audio load result handler

--- src/main/java/service/music/AudioLoadResultHandlerKeyword.java
+++ src/main/java/service/music/AudioLoadResultHandlerKeyword.java
@@ -56,39 +56,40 @@ public class AudioLoadResultHandlerKeyword implements AudioLoadResultHandler {
 
     @Override
     public void playlistLoaded(AudioPlaylist playlist) {
-        AudioTrack track = playlist.getSelectedTrack();
-        if (track == null) {
-            track = playlist.getTracks().get(0);
-        }
-        if (track.getDuration() > 900000) {
-            String errorMessage = ":negative_squared_cross_mark: | cannot load song with duration longer than 15 minutes";
-            channel.sendMessage(errorMessage).queue();
-            return;
-        }
-        track.setUserData(requester);
+        for (AudioTrack track : playlist.getTracks()) {
+            if (track.getDuration() > 900000) {
+                continue;
+            }
+            track.setUserData(requester);
 
-        musicManager.scheduler.queue(track);
+            musicManager.scheduler.queue(track);
 
-        PremiumService.addHistory(playlist.getName(), track.getInfo().uri,
-                requester.getGuild(), requester.getUser());
+            PremiumService.addHistory(playlist.getName(), track.getInfo().uri,
+                    requester.getGuild(), requester.getUser());
+
+            if (channel != null) {
+                int positionInQueue = musicManager.scheduler.getQueue().size();
+
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+                embedBuilder.setColor(requester.getColor());
+                embedBuilder.setDescription("\uD83C\uDFB5 [" + track.getInfo().title + "](" + track.getInfo().uri + ")");
+
+                embedBuilder.setAuthor("Added to queue", requester.getUser().getEffectiveAvatarUrl(),
+                        requester.getUser().getEffectiveAvatarUrl());
+
+                embedBuilder.addField("Channel", track.getInfo().author, true);
+                embedBuilder.addField("Song Duration", MusicUtils.getDurationFormat(track.getDuration()), true);
+                embedBuilder.addField("Position in queue", "" + positionInQueue, true);
+                embedBuilder.addField("Estimated time until playing",
+                        musicManager.getEstimatedTimeUntilPlaying(positionInQueue), true);
+
+                channel.sendMessage(embedBuilder.build()).queue();
+            }
+        }
 
         if (channel != null) {
-            int positionInQueue = musicManager.scheduler.getQueue().size();
-
-            EmbedBuilder embedBuilder = new EmbedBuilder();
-            embedBuilder.setColor(requester.getColor());
-            embedBuilder.setDescription("\uD83C\uDFB5 [" + track.getInfo().title + "](" + track.getInfo().uri + ")");
-
-            embedBuilder.setAuthor("Added to queue", requester.getUser().getEffectiveAvatarUrl(),
-                    requester.getUser().getEffectiveAvatarUrl());
-
-            embedBuilder.addField("Channel", track.getInfo().author, true);
-            embedBuilder.addField("Song Duration", MusicUtils.getDurationFormat(track.getDuration()), true);
-            embedBuilder.addField("Position in queue", "" + positionInQueue, true);
-            embedBuilder.addField("Estimated time until playing",
-                    musicManager.getEstimatedTimeUntilPlaying(positionInQueue), true);
-
-            channel.sendMessage(embedBuilder.build()).queue();
+            String errorMessage = ":negative_squared_cross_mark: | cannot load song with duration longer than 15 minutes";
+            channel.sendMessage(errorMessage).queue();
         }
     }
 
