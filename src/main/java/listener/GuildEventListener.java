57b141acfe3ed5c70545ede0c4e355276056077a
MadeYoga <vngla21@gmail.com>
2020-11-15 12:22:03 +0700

    Update Guild Event Listener

--- src/main/java/listener/GuildEventListener.java
+++ src/main/java/listener/GuildEventListener.java
@@ -2,12 +2,14 @@ package listener;
 
 import com.jagrosh.jdautilities.command.CommandClient;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.discordbots.api.client.DiscordBotListAPI;
 import org.jetbrains.annotations.NotNull;
+import service.music.CustomEmbedBuilder;
 
 public class GuildEventListener extends ListenerAdapter {
 
@@ -25,6 +27,25 @@ public class GuildEventListener extends ListenerAdapter {
 
         this.dblApi.setStats(jda.getGuilds().size());
 
+        TextChannel defaultTextChannel = event.getGuild().getDefaultChannel();
+        if (defaultTextChannel == null) {
+            return;
+        }
+
+        CustomEmbedBuilder embedBuilder = new CustomEmbedBuilder();
+
+        embedBuilder.setThumbnail(event.getJDA().getSelfUser().getAvatarUrl());
+        embedBuilder.setTitle(":headphones: | Thanks for using Musicfy!");
+        embedBuilder.setDescription("Please use `m$help` to get started!");
+
+        String links = "";
+        links += "[Musicfy Invite Link](https://discord.com/api/oauth2/authorize?client_id=473023109666963467&permissions=36793408&scope=bot)\n";
+        links += "[Wiki](https://github.com/nano-devs/Musicfy-Bot/wiki)\n";
+        links += "[Patreon](https://www.patreon.com/musicfy)";
+        embedBuilder.addField("Links", links, false);
+        embedBuilder.setFooter("Have a nice dayy~");
+
+        defaultTextChannel.sendMessage(embedBuilder.build()).queue();
         // Notify owner. development purposes
 //        String messageToOwner = "Just left server " + event.getGuild().getName()
 //                + ". Total " + jda.getGuilds().size() + " guilds";
