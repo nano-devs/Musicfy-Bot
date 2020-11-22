7942987f78150b34d1d3d9e757ecad2a59ffee08
MadeYoga <vngla21@gmail.com>
2020-11-22 16:20:44 +0700

    Update webhook

--- src/main/java/service/music/filter/BassBoostFilter.java
+++ src/main/java/service/music/filter/BassBoostFilter.java
@@ -1,16 +1,21 @@
 package service.music.filter;
 
 import com.sedmelluq.discord.lavaplayer.filter.equalizer.EqualizerFactory;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 
-public class CustomFilter {
+public class BassBoostFilter {
     private static final float[] BASS_BOOST = { 0.2f, 0.15f, 0.1f, 0.05f, 0.0f, -0.05f, -0.1f, -0.1f, -0.1f, -0.1f,
             -0.1f, -0.1f, -0.1f, -0.1f, -0.1f };
 
     private final EqualizerFactory equalizer;
+    private final AudioPlayer player;
 
-    public CustomFilter() {
+    public BassBoostFilter(AudioPlayer player) {
         this.equalizer = new EqualizerFactory();
+        this.player = player;
     }
 
+    public void applyFilters() {
 
+    }
 }
